9ac9ab26ca80f5f53f44cab46512db8053ef1c1d
unknown <agiljibrin009@gmail.com>
2024-01-24 22:41:06 +0800

    fix: memperbaiki tampilan form pendaftaran smk

--- resources/views/landing/core.blade.php
+++ resources/views/landing/core.blade.php
@@ -9,6 +9,7 @@
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
     <title>SMK PERSADA</title>
 </head>
 
@@ -19,15 +20,15 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('jquery.js') }}"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
     <script>
-        $(".tekan").on( "click", function() {
-            if ( $( ".list" ).first().is( ":hidden" ) ) {
-                $( ".list" ).slideDown();
+        $(".tekan").on("click", function() {
+            if ($(".list").first().is(":hidden")) {
+                $(".list").slideDown();
             } else {
-                $( ".list" ).slideUp();
-            }}
-            );
+                $(".list").slideUp();
+            }
+        });
     </script>
 </body>
 
