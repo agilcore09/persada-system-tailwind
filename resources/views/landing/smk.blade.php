596eb512c24bfa4125d61066c03c005e5fef4cdd
unknown <agiljibrin009@gmail.com>
2024-01-24 21:24:56 +0800

    feat: membuat commit pendaftar untuk di masukkan ke database

--- resources/views/landing/smk.blade.php
+++ resources/views/landing/smk.blade.php
@@ -18,7 +18,6 @@
             </div>
             <div class="form mt-14">
                 <form class="max-w-md mx-auto">
-
                     <div class="flex items-center w-full">
                         <label for="dropzone-file"
                             class="flex flex-col items-center justify-center w-64 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 hover:bg-gray-100">
@@ -38,7 +37,6 @@
                                 onchange="previewImage(event);" />
 
                         </label>
-
                         <div class="preview w-64 h-64 border-2 border-gray-300 border-dashed rounded-l">
                             <img id="preview-selected-image" />
                         </div>
@@ -51,8 +49,8 @@
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                 placeholder=" " required />
                             <label for="floating_first_name"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First
-                                name</label>
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama
+                                Depan</label>
                         </div>
                         <div class="relative z-0 w-full mb-5 group">
                             <input type="text" name="floating_last_name" id="floating_last_name"
