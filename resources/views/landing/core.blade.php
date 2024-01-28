3e0bb5ed997145bc301916fec6cbb7ce2c142555
unknown <agiljibrin009@gmail.com>
2024-01-28 16:27:09 +0800

    fix: menambah aos

--- resources/views/landing/core.blade.php
+++ resources/views/landing/core.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>SMK PERSADA</title>
 </head>
 
@@ -24,7 +25,10 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('jquery.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
+        AOS.init();
+
         $(".tekan").on("click", function() {
             if ($(".list").first().is(":hidden")) {
                 $(".list").slideDown();
--- resources/views/landing/homepage.blade.php
+++ resources/views/landing/homepage.blade.php
@@ -14,16 +14,16 @@
     {{-- start why --}}
     <div class="grid md:grid-cols-12 grid-cols-12">
         <div class="col-span-10 col-start-2 font-body">
-            <div class="kenapa mt-14">
+            <div class="kenapa mt-14" data-aos="fade-up">
                 <h3 class="font-bold text-2xl text-center">Kenapa Harus <span class="text-blue-950 font-bold">SMK PERSADA
                         MAKASSAR</span> ? </h3>
                 <p class="text-center text-lg">alasan kenapa harus memilih untuk mendaftar di <span
                         class="text-blue-950 font-bold">SMK PERSADA MAKASSAR</span></p>
             </div>
-            <div class="alasan grid md:grid-cols-12 grid-cols-12 mt-12">
+            <div class="alasan grid md:grid-cols-12 grid-cols-12 mt-12" data-aos="fade-up">
                 <div class="col-span-12 my-5 px-9 md:col-span-3">
                     <div class="icons flex justify-center">
-                        <i class="fa-solid fa-handshake fa-3x text-blue-950"></i>
+                        <i class="fa-solid fa-handshake fa-3x text-blue-950 hover:text-blue-900"></i>
                     </div>
                     <h3 class="text-center text-blue-950 font-bold text-xl mt-3 font-body">kerja Sama Luas</h3>
                     <p class="text-center text-sm font-body">Kami telah bekerja sama dengan beberapa perusahaan terkenal dan
@@ -31,7 +31,7 @@
                 </div>
                 <div class="col-span-12 my-5 px-9 md:col-span-3 ">
                     <div class="icons flex justify-center">
-                        <i class="fa-solid fa-laptop fa-3x text-blue-950"></i>
+                        <i class="fa-solid fa-laptop fa-3x text-blue-950 hover:text-blue-900"></i>
                     </div>
                     <h3 class="text-center text-blue-950 font-bold text-xl mt-3 font-body">Fasilitas Lengkap</h3>
                     <p class="text-center text-sm font-body">Penunjang pembelajaran dan praktikum dengan kualitas yang
@@ -39,14 +39,14 @@
                 </div>
                 <div class="col-span-12 my-5 px-9 md:col-span-3">
                     <div class="icons flex justify-center">
-                        <i class="fa-solid fa-school fa-3x text-blue-950"></i>
+                        <i class="fa-solid fa-school fa-3x text-blue-950 hover:text-blue-900"></i>
                     </div>
                     <h3 class="text-center text-blue-950 font-bold text-xl mt-3 font-body">Lingkungan Yang Nyaman</h3>
                     <p class="text-center text-sm font-body">Berada di lingkungan yang kondusif dan asri</p>
                 </div>
                 <div class="col-span-12 my-5 px-9 md:col-span-3">
                     <div class="icons flex justify-center ">
-                        <i class="fa-solid fa-person-chalkboard fa-3x text-blue-950"></i>
+                        <i class="fa-solid fa-person-chalkboard fa-3x text-blue-950 hover:text-blue-900"></i>
                     </div>
                     <h3 class="text-center text-blue-950 font-bold text-xl mt-3 font-body">Pengajar Kompoten</h3>
                     <p class="text-center text-sm font-body">Kami telah bekerja sama dengan beberapa perusahaan terkenal dan
@@ -63,7 +63,7 @@
         <div class="col-span-10 col-start-2">
             <div class="grid grid-cols-10">
                 <div class="col-span-10 md:col-span-5 py-14">
-                    <div class="container font-body">
+                    <div class="container font-body" data-aos="fade-right">
                         <div>
                             <h3 class="text-3xl font-bold pt-14 mb-5">Assalamualaikum Wr. Wb</h3>
                         </div>
@@ -91,7 +91,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-span-10 md:col-span-5 pt-14">
+                <div class="col-span-10 md:col-span-5 pt-14" data-aos="fade-left">
                     <img src="{{ asset('kepsek.png') }}" alt="kepsek.jpg">
                 </div>
 
@@ -103,23 +103,23 @@
     {{-- jurusan --}}
     <div class="grid md:grid-cols-12 grid-cols-12 my-14">
         <div class="col-span-10 col-start-2">
-            <div class="mt-14">
+            <div class="mt-14" data-aos="fade-up">
                 <h3 class="font-bold text-2xl text-center">Program <span class="text-blue-950 font-bold">Keahlian</span>
                 </h3>
                 <p class="text-center text-sm">Pilih Program keahlian yang kamu sukai</p>
             </div>
             <div class="col-span-10">
                 <div class="grid md:grid-cols-12 grid-cols-12">
-                    <div class="col-span-12 sm:col-span-12 p-10 md:col-span-3 md:col-start-4">
+                    <div class="col-span-12 sm:col-span-12 p-10 md:col-span-3 md:col-start-4" data-aos="fade-right">
                         <div class="icons flex justify-center ">
-                            <i class="fa-solid fa-server fa-3x text-blue-950"></i>
+                            <i class="fa-solid fa-server fa-3x hover:text-blue-900 text-blue-950"></i>
                         </div>
                         <h3 class="text-center text-blue-950 font-bold text-lg mt-3">Teknik Komputer Jaringan</h3>
                         <p class="text-center text-sm">Berfokus Pada Perangkat Jaringan, dan Pemrograman komputer</p>
                     </div>
-                    <div class="col-span-12 sm:col-span-12 md:col-span-3 p-10">
+                    <div class="col-span-12 sm:col-span-12 md:col-span-3 p-10" data-aos="fade-left">
                         <div class="icons flex justify-center ">
-                            <i class="fa-solid fa-gear fa-3x text-blue-950"></i>
+                            <i class="fa-solid fa-gear fa-3x text-blue-950 hover:text-blue-900"></i>
                         </div>
                         <h3 class="text-center text-blue-950 font-bold text-lg mt-3">Teknik Kendaraan Ringan</h3>
                         <p class="text-center text-sm">Berfokus Pada Pemeliharaan dan Perawatan mesin kendaraan ringan</p>
@@ -133,71 +133,35 @@
     {{-- gallery --}}
     <div class="grid md:grid-cols-12 grid-cols-12 my-14">
         <div class="col-span-10 col-start-2">
-            <div class="mt-14">
+            <div class="mt-14" data-aos="fade-up">
                 <h3 class="font-bold text-2xl text-center">Gallery <span class="text-blue-950 font-bold">Kami</span>
                 </h3>
                 <p class="text-center text-sm">Beberapa foto kegiatan yang di lakukan siswa</p>
             </div>
             <div class="col-span-10 mt-14">
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-                    <div class="grid gap-4">
-                        <div>
-                            <a data-fslightbox="first-lightbox" href="{{ asset('sertivikasi/mikrotik.jpg') }}">
-                                <img class="h-auto max-w-full rounded-lg hover:grayscale"
-                                    src="{{ asset('sertivikasi/mikrotik.jpg') }}" alt="">
-                            </a>
+                    <?php
+                    $files = scandir('../public/gallery');
+                    
+                    // Menghapus nilai ". dan .."
+                    $filteredFiles = array_filter($files, function ($value) {
+                        return $value !== '.' && $value !== '..';
+                    });
+                    
+                    // Reindeks array jika diperlukan
+                    $filteredFiles = array_values($filteredFiles); ?>
+
+                    @foreach ($filteredFiles as $file)
+                        <div class="grid gap-4 ">
+                            <div>
+                                <a data-fslightbox="first-lightbox" href="{{ asset('/gallery' . '/' . $file) }}">
+                                    <img class="max-w-full rounded-lg hover:grayscale"
+                                        src="{{ asset('/gallery' . '/' . $file) }}" alt="">
+                                </a>
+                            </div>
                         </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg  hover:grayscale"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="grid gap-4">
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="grid gap-4">
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="grid gap-4">
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
-                        </div>
-                    </div>
+                    @endforeach
+
                 </div>
 
             </div>
@@ -208,7 +172,7 @@
 
     <div class="grid md:grid-cols-12 grid-cols-12 my-14">
         <div class="col-span-10 col-start-2">
-            <div class="mt-14">
+            <div class="mt-14" data-aos="fade-up">
                 <h3 class="font-bold text-2xl text-center">Sertivikasi<span class="text-blue-950 font-bold">
                         Internasional</span>
                 </h3>
@@ -220,8 +184,7 @@
             <div class="grid md:grid-cols-12 grid-cols-12">
                 <div class="col-span-6 sm:col-span-3 md:col-span-3 my-5">
                     <div class="w-full flex justify-center self-end p-5">
-                        <img src="{{ asset('/sertivikasi/mikrotik.jpg') }}" class="w-full hover:grayscale"
-                            alt="">
+                        <img src="{{ asset('/sertivikasi/mikrotik.jpg') }}" class="w-full hover:grayscale" alt="">
                     </div>
                     <h3 class="font-bold text-center">Mikrotik Academy</h3>
                 </div>
