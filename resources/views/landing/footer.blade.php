1571b1f02f80cb0323859b63b59536cd02767091
fuureya <agiljibrin009@gmail.com>
2024-05-08 21:47:29 +0800

    fix: text jumbotron

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-blue-950 shadow">
+{{-- <footer class="bg-blue-950 shadow">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="{{ url('/') }}" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
@@ -23,5 +23,69 @@
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{ url('/') }}"
                 class="hover:underline">Persadaschool™</a>. All Rights Reserved.</span>
+    </div>
+</footer> --}}
+
+<footer class="bg-blue-950">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <h2 class="text-center md:text-2xl text-xl text-white font-bold">Come Join With Us</h2>
+        <p class="text-white px-10 text-sm  text-justify md:text-center">Gabunglah bersama kami di SMK Persada Makassar
+            untuk
+            meniti
+            jalan
+            sukses
+            dalam
+            program
+            unggulan Teknik
+            Komputer Jaringan dan Teknik Kendaraan Ringan!</p>
+        <div class="grid grid-cols-12 mt-8">
+            <div class="md:col-span-7 col-span-12">
+                <h4 class="text-center text-white font-bold text-md  mb-3">Ajukan Pertanyaan</h4>
+                <div class="container px-5">
+                    <div>
+                        <input class="w-full rounded-md placeholder:text-sm" type="text" name=""
+                            placeholder="Nama Kamu" required autocomplete="off">
+                    </div>
+                    <div class="py-3">
+                        <input class="w-full rounded-md placeholder:text-sm" type="text" name=""
+                            placeholder="Pertanyaan Kamu" required autocomplete="off">
+                    </div>
+                    <button class="w-full bg-orange-600 rounded-md py-2 font-bold text-white">Submit</button>
+                </div>
+            </div>
+
+            <div class=" md:col-span-2 col-span-4 mt-5 md:mt-0">
+                <h4 class="text-center text-white font-bold text-md mb-3">Our Service</h4>
+                <ul class="text-center">
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Home</a></li>
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">About</a></li>
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Contact Us</a></li>
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Ujian</a></li>
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Persada Exam </a></li>
+                </ul>
+            </div>
+            <div class="md:col-span-2  col-span-4 mt-5 md:mt-0">
+                <h4 class="text-center text-white font-bold text-md  mb-3">Join With Us</h4>
+                <ul class="text-center">
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Daftar SMK</a></li>
+                    <li><a href="{{ url('/') }}" class="text-slate-200 text-sm">Daftar SMP</a></li>
+
+                </ul>
+            </div>
+            <div class="md:col-span-1 col-span-4 mt-5 md:mt-0 flex justify-center ">
+                <ul class="text-center content-center">
+                    <li class="py-3">
+                        <a href="{{ url('/') }}" class="text-slate-200"><i
+                                class="fa-brands fa-2xl fa-instagram"></i></a>
+                    </li>
+                    <li class="py-3"><a href="{{ url('/') }}" class="text-slate-200"><i
+                                class="fa-brands fa-2xl fa-facebook"></i></a>
+                    <li class="py-3"><a href="{{ url('/') }}" class="text-slate-200"><i
+                                class="fa-brands fa-2xl fa-whatsapp"></i></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
--- resources/views/landing/jumbotron.blade.php
+++ resources/views/landing/jumbotron.blade.php
@@ -1,10 +1,10 @@
 <div class="grid md:grid-cols-12 grid-cols-12" id="home">
     <section class="col-span-12 font-body bg-center bg-no-repeat  bg-jumbotron bg-cover">
         <div class="px-4 mx-auto text-center py-24 lg:py-56">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+            <h1 class="mb-4 text-2xl md:text-5xl lg:text-6xl font-extrabold tracking-tight leading-none text-white ">
                 SMK
                 PERSADA MAKASSAR</h1>
-            <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Tumbuh Bersama, Berkarya
+            <p class="mb-8 text-md font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Tumbuh Bersama, Berkarya
                 Bersama SMK PERSADA MAKASSAR</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                 <a href="#"
@@ -17,7 +17,7 @@
                     </svg>
                 </a>
                 <a href="#"
-                    class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                    class="inline-flex justify-center hover:text-gray-900 items-center py-1 md:py-3 px-3 md:px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
                     Hubungi Kami
                 </a>
             </div>
