0bc220fbe0b4db767debd437e633c58b391af763
fuureya <agiljibrin009@gmail.com>
2024-05-10 11:46:45 +0800

    fix: form contact us

--- resources/views/landing/contact_us.blade.php
+++ resources/views/landing/contact_us.blade.php
@@ -6,38 +6,31 @@
     {{-- vISI --}}
     <div class="grid md:grid-cols-12 grid-cols-12 my-14 md:my-7">
         <div class="col-span-10 col-start-2">
-            <div class="md:mt-5" data-aos="fade-up">
-                <h3 class="font-bold text-2xl text-center">SMK <span class="text-blue-950 font-bold">PERSADA MAKASSAR</span>
-                </h3>
-                <p class="md:text-center text-sm text-justify">SMK Persada menonjol dengan keunggulan dualitas program
-                    SMK Persada Makassar, sebuah lembaga pendidikan kejuruan yang menonjol, menawarkan program-program
-                    unggulan dalam Teknik Kendaraan Ringan (TKR) dan Teknik Komputer Jaringan (TKJ), memberikan siswa-siswa
-                    kesempatan untuk mengembangkan keterampilan teknis yang relevan dan siap pakai dalam industri masa
-                    depan.</p>
-            </div>
+
             <div class="col-span-10">
                 <div class="grid md:grid-cols-12 grid-cols-12">
-                    <div class="col-span-12 md:col-span-6 my-5  " data-aos="fade-up">
+                    <div class="col-span-12 md:col-span-6 my-5 border border-black" data-aos="fade-up">
                         <div class="grid grid-cols-12 md:col-span-6">
-                            <div class="col-span-12 md:col-span-6 icons flex justify-center">
-                                <div>
-                                    <h3>Contact US</h3>
+                            <div class="col-span-12 md:col-span-12 icons px-5">
+                                <div class="header">
+                                    <h3 class="font-bold text-xl text-center">CONTACT US</h3>
+                                    <p class="md:text-center text-sm text-center">Temui kamu di beberpa paltform dan media
+                                        social. Kamu dapat mengajukan pertanyaan atau menyapa kami. </p>
                                 </div>
-                            </div>
-                            <div class="col-span-12 md:col-span-6 md:flex ">
-                                <div class="md:self-center">
-                                    <h3
-                                        class=" px-3 md:px-0 text-white font-bold text-xl md:text-2xl mt-3 font-body  text-center ">
-                                        VISI KAMI</h3>
-                                    <p class=" text-sm font-body px-3 md:px-0 text-center  md:text-center text-white">
-                                        Klik untuk melihat lengkap</p>
-                                    <div class="flex justify-center">
-                                        <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
-                                            class="bg-white px-7 py-1 rounded-md mt-4 ml-3 font-bold hover:bg-blue-800 hover:text-white">Lihat
-                                            Visi</button>
-                                    </div>
+                                <div class="form-body">
+                                    <input type="text" name="nama" placeholder="Nama Kamu"
+                                        class="w-full border border-t-0 border-x-0 placeholder:text-center focus:text-center"
+                                        required autocomplete="off">
+                                    <input type="email" name="email" placeholder="Email Kamu ex: azizi@gmail.com"
+                                        class="w-full border border-t-0 border-x-0 placeholder:text-center focus:text-center mt-5"
+                                        autocomplete="off" required>
+                                    <input type="text" name="pertanyaan" placeholder="Pertanyaan Kamu"
+                                        class="w-full border border-t-0 border-x-0 placeholder:text-center focus:text-center mt-5"
+                                        autocomplete="off" required>
                                 </div>
+
                             </div>
+
                         </div>
                     </div>
                     <div class="col-span-12 md:col-span-6 md:mx-2 my-5  bg-blue-950 rounded-md py-14 hover:bg-blue-900 hover:border-none shadow-lg "
