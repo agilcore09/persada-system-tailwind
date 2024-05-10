2f11eeb0f20723924882c9ac67ae40d78d2444bd
fuureya <agiljibrin009@gmail.com>
2024-05-10 20:31:55 +0800

    fix: perbaikan about-us

--- resources/views/landing/contact_us.blade.php
+++ resources/views/landing/contact_us.blade.php
@@ -13,7 +13,7 @@
                         <div class="grid grid-cols-12 md:col-span-6">
                             <div class="col-span-12 md:col-span-12 icons px-5">
                                 <div class="header">
-                                    <h3 class="font-bold text-xl text-center mb-3">CONTACT US</h3>
+                                    <h3 class="font-bold text-xl text-center mb-3">ADA PERTANYAAN ? </h3>
                                     <p class="md:text-center text-sm text-center mb-3">Temui kamu di beberpa paltform dan
                                         media
                                         social. Kamu dapat mengajukan pertanyaan atau menyapa kami. </p>
@@ -38,19 +38,40 @@
                     </div>
                     <div class="col-span-12 md:col-span-6 md:mx-2 my-5  bg-blue-950 rounded-md py-14 hover:bg-blue-900 hover:border-none shadow-lg"
                         data-aos="fade-up">
-
                         <div class="social-media-header">
                             <h3 class="font-bold text-xl text-center mb-3 text-white">CONTACT US</h3>
-                            <ul class="pl-10">
-                                <li>
-                                    <a href="#" class="flex">
-                                        <i class="fa-solid fa-envelope text-white pr-2 self-center"> </i>
-                                        <p class="font-bold text-white self-center">smkspersada@gmail.com</p>
-                                    </a>
-                                </li>
-                                <li><a href="#"><i class="fa-solid fa-phone text-white"></i></a></li>
-                                <li><a href="#"><i class="fa-brands fa-instagram text-white"></i></a></li>
-                            </ul>
+                            <div class="flex justify-center">
+                                <ul class="pl-10">
+                                    <li>
+                                        <a href="#" class="flex py-3">
+                                            <i class="fa-solid fa-envelope font-xl text-white pr-2 self-center"> </i>
+                                            <p class="font-bold text-white self-center md:text-md text-sm">
+                                                smkspersada@gmail.com</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex py-3">
+                                            <i class="fa-brands fa-whatsapp font-xl text-white pr-2 self-center"></i>
+                                            <p class="font-bold text-white self-center md:text-md text-sm">+62 823-9311-8939
+                                            </p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex py-3">
+                                            <i class="fa-solid fa-phone font-xl text-white pr-2 self-center"></i>
+                                            <p class="font-bold text-white self-center md:text-md text-sm">+62 823-9311-8939
+                                            </p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex py-3">
+                                            <i class="fa-brands fa-instagram font-xl text-white pr-2 self-center"></i>
+                                            <p class="font-bold text-white self-center md:text-md text-sm">
+                                                smkspersada@gmail.com</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
 
                     </div>
