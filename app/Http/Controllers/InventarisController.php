49759ac7a74ce6200466f26320f038b618cf9379
fuureya <agiljibrin009@gmail.com>
2024-05-11 09:14:57 +0800

    feat: membuat controller, migrasi, model untuk inventaris

---
+++ app/Http/Controllers/InventarisController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InventarisModel;
+use Illuminate\Http\Request;
+
+class InventarisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InventarisModel  $inventarisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InventarisModel $inventarisModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InventarisModel  $inventarisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InventarisModel $inventarisModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InventarisModel  $inventarisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InventarisModel $inventarisModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InventarisModel  $inventarisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InventarisModel $inventarisModel)
+    {
+        //
+    }
+}
