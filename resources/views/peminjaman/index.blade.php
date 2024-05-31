242781d63a57773deb5b527f208b6cb342eb1d92
fuureya <agiljibrin009@gmail.com>
2024-05-31 21:08:36 +0800

    fix: mengubah nama placeholder

--- resources/views/peminjaman/index.blade.php
+++ resources/views/peminjaman/index.blade.php
@@ -86,7 +86,8 @@
                     <div class="pt-2 relative mx-auto text-gray-900">
                         <input
                             class="border-2 border-gray-900 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                            type="search-nis" name="search" placeholder="Cari Nama" autocomplete="off" id="search-nis">
+                            type="search-nis" name="peminjam" placeholder="Cari Nama Peminjam" autocomplete="off"
+                            id="peminjam">
                         <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
                             <svg class="text-gray-900 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                 xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
