b0f962d2070b1fc797e81ef0d12ec383d1c25795
fuureya <agiljibrin009@gmail.com>
2024-05-31 21:05:56 +0800

    feat: menambah stylesheet icon di head html core  di dashboard

--- resources/views/core/index.blade.php
+++ resources/views/core/index.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
     <title>SMK PERSADA</title>
     @vite('resources/css/app.css')
 </head>
