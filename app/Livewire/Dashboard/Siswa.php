f096de1f60f1ae58f93c40883f2ab06fb5367932
fuureya <agiljibrin009@gmail.com>
2024-09-12 19:45:06 +0100

    feat: migrasi fitur ajax cari menggunakan livewire livesearch

--- app/Livewire/Dashboard/Siswa.php
+++ app/Livewire/Dashboard/Siswa.php
@@ -9,11 +9,17 @@ use Livewire\Component;
 
 class Siswa extends Component
 {
+
+    public $search;
     public function render()
     {
         $jurusan = CategoryModel::all();
         $kelasSiswa = TypeModel::all();
         $data = SiswaModel::with(['Category', 'Type'])->get();
+
+        if ($this->search != null) {
+            $data = SiswaModel::where('nama_siswa', 'like', '%' . $this->search . '%')->get();
+        }
         return view('livewire.dashboard.siswa', compact("data", "jurusan", "kelasSiswa"));
     }
 }
